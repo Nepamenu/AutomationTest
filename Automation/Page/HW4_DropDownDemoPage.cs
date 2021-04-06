5fbaa79b3898a4eea52926ae0becd0d0ac43dca4
JB <juratba@gmail.com>
2021-04-06 22:07:59 +0300

    Added HW4 Lesson 4 Drop Down

---
+++ Automation/Page/HW4_DropDownDemoPage.cs
@@ -0,0 +1,85 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automation.Page
+{
+    public class HW4_DropDownDemoPage : BasePage
+    {
+        private const string _pageAddress = "https://www.seleniumeasy.com/test/basic-select-dropdown-demo.html";
+        private const string _firstSelectedPrefixText = "First selected option is : ";
+        private const string _multiSelectedPrefixText = "Options selected are : ";
+
+        private SelectElement _multiDropDown => new SelectElement(Driver.FindElement(By.Id("multi-select")));
+        private IWebElement _firstSelectedButton => Driver.FindElement(By.Id("printMe"));
+        private IWebElement _getAllSelected => Driver.FindElement(By.Id("printAll"));
+        private IWebElement _multiResultText => Driver.FindElement(By.CssSelector(".getall-selected"));
+
+
+        public HW4_DropDownDemoPage(IWebDriver webdriver) : base(webdriver)
+        {
+            Driver.Url = _pageAddress;
+        }
+
+        public HW4_DropDownDemoPage SelectFromMultipageDropDown(List<string> statesLites)
+        {
+            Actions action = new Actions(Driver);
+            action.KeyDown(Keys.Control);
+
+            foreach (string state in statesLites)
+            {
+                foreach (IWebElement option in _multiDropDown.Options)
+                {
+                    if (state.Equals(option.GetAttribute("value")))
+                    {
+                        option.Click();
+                    }
+                }
+
+            }
+
+            action.KeyUp(Keys.Control);
+            action.Build().Perform();
+            return this;
+        }
+
+        public HW4_DropDownDemoPage ClickFirstSelectedButton()
+        {
+            _firstSelectedButton.Click();
+            return this;
+        }
+
+        public HW4_DropDownDemoPage ClickGetAllSelectedButton()
+        {
+            _getAllSelected.Click();
+            return this;
+        }
+
+
+        public HW4_DropDownDemoPage VerifyFirstSelectedState(string firstState)
+        {
+            Assert.AreEqual(_firstSelectedPrefixText + firstState, _multiResultText.Text, "Different first State");
+            return this;
+        }
+
+        public HW4_DropDownDemoPage VerifyAllSelectedState(List<string> statesLites)
+        {
+            foreach (string state in statesLites)
+            {
+                foreach (IWebElement option in _multiDropDown.Options)
+                {
+                    Assert.AreEqual(_multiSelectedPrefixText + option, _multiResultText.Text, "Different selected States");
+                }
+
+            }
+
+            return this;
+        }
+    }
+}
---
+++ Automation/Test/HW4_DropDownDemoTest.cs
@@ -0,0 +1,53 @@
+﻿using Automation.Page;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automation.Test
+{
+    public class HW4_DropDownDemoTest
+    {
+        private static HW4_DropDownDemoPage _page;
+        [OneTimeSetUp]
+
+        public static void Setup()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            _page = new HW4_DropDownDemoPage(driver);
+        }
+
+        [OneTimeTearDown]
+        public static void TearDown()
+        {
+            // _page.CloseBrowser();
+        }
+
+        [TestCase("Texas", "Florida", TestName = "First selected Texas")]
+        [TestCase("California", "Pennsylvania", "Ohio", TestName = "First selected California")]
+        
+        public static void MultiplyDropDownFirstSelectedTest(params string[] states)
+        {
+            _page.SelectFromMultipageDropDown(states.ToList())
+                .ClickFirstSelectedButton()
+                .VerifyFirstSelectedState(states[0]);
+        }
+
+        [TestCase("California", "Washington", TestName = "Selected states California, Washington")]
+        [TestCase("New Jersey", "California", "Washington", "Texas", TestName = "Selected states New Jersey, California, Washington, Texas")]
+
+        public static void MultiplyDropDownAllSelectedTest(params string[] states)
+        {
+            _page.SelectFromMultipageDropDown(states.ToList())
+                .ClickGetAllSelectedButton()
+                .VerifyAllSelectedState(states.ToList());
+        }
+
+    }
+}
